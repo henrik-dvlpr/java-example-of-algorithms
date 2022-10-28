f2f59e1f471c865fa410e0114a7e01257c85d6de
Henrik Adamski <h.adamski@icloud.com>
2022-10-28 10:18:18 +0200

    Add runtime tracking

--- src/main/java/de/github/henrik_dvlpr/Main.java
+++ src/main/java/de/github/henrik_dvlpr/Main.java
@@ -4,7 +4,11 @@ import de.github.henrik_dvlpr.algorithm.BinarySearch;
 import de.github.henrik_dvlpr.test_data.FindUniversities;
 import de.github.henrik_dvlpr.test_data.University;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalField;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.net.http.HttpResponse.BodyHandlers.ofString;
 
@@ -13,24 +17,38 @@ public class Main {
         // loading test data
         final var universities = new FindUniversities().find();
 
-        testBinarySearch(universities);
+        // test binary search
+        runTestBinarySearch(universities);
     }
 
-    private static void testBinarySearch(List<University> universities) {
-        // sorting list
+    private static void runTestBinarySearch(final List<University> universities) {
+        // convert to sorted university names list
         final var sortedListOfUniversities = universities.stream()
                 .map(University::getName).sorted(Comparator.naturalOrder()).toList();
+        // binary search and track runtime
+        trackRuntime("binary search", () -> testBinarySearch(sortedListOfUniversities));
+    }
+
+    private static void testBinarySearch(final List<String> universityNames) {
         // using binary search
         final var indexOfSearchedValue = new BinarySearch<String>()
-                .indexOf(sortedListOfUniversities, "Massachusetts Institute of Technology");
+                .indexOf(universityNames, "Massachusetts Institute of Technology");
 
-        System.out.println("Total items: " + sortedListOfUniversities.size());
+        System.out.println("Total items: " + universityNames.size());
 
         if (indexOfSearchedValue.isPresent()) {
-            System.out.println("Value at index " + indexOfSearchedValue.get() + " is " +
-                    sortedListOfUniversities.get(indexOfSearchedValue.get()));
+            System.out.println("Value at index " + indexOfSearchedValue.get() + " is " + universityNames.get(indexOfSearchedValue.get()));
         } else {
             System.out.println("Not found in list!");
         }
+    }
+
+    private static void trackRuntime(final String name, final Runnable runnable) {
+        final var startTimeMillis = System.currentTimeMillis();
+        runnable.run();
+        final var endTimeMillis = System.currentTimeMillis();
+
+        long runtimeMillis = endTimeMillis - startTimeMillis;
+        System.out.println("Total runtime of '" + name + "': " + runtimeMillis + "ms");
     }
 }
